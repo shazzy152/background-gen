bd02617e6bd917f1b3d12fa558adbb9c86f4dadd
shazzy152 <44363342+shazzy152@users.noreply.github.com>
2020-12-26 22:48:43 +0530

    Update new.js

--- new.js
+++ new.js
@@ -2,6 +2,7 @@ var h3 = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.querySelector("body");
+var button1 = document.querySelector(".Random");
 
 function setGradient(){
 	body.style.background = 
@@ -12,7 +13,20 @@ function setGradient(){
 		+ ")";
 		h3.textContent = "Left : " + color1.value + " " + "Right : " + color2.value;
 }
+function getRandomColor(){
+	var randomColor1 = Math.floor(Math.random()*16777215).toString(16);
+	var rC1 = "#" + randomColor1
+
+	return rC1;
+}
+function setRandomColor(){
+	color1.value = getRandomColor();
+	color2.value = getRandomColor();
+	setGradient();
+}
 
 color1.addEventListener("input",setGradient)
 
 color2.addEventListener("input",setGradient)
+
+button1.addEventListener("click",setRandomColor)
